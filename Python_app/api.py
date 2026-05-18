743c0789123bc9575a664ed3fb91de6038a59612
Liza-Khube <elisekhu@gmail.com>
2026-05-19 01:52:11 +0300

    Add files for python app section

---
+++ Python_app/api.py
@@ -0,0 +1,19 @@
+import numpy as np
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get('')
+def hello_world() -> dict:
+    return {'msg': 'Hello, World!'}
+
+@router.get('/matrix')
+def matrix_multiply() -> dict:
+    matrix_a = np.random.rand(10, 10)
+    matrix_b = np.random.rand(10, 10)
+    product = np.dot(matrix_a, matrix_b)
+    return {
+        'matrix_a': matrix_a.tolist(),
+        'matrix_b': matrix_b.tolist(),
+        'product': product.tolist(),
+}
